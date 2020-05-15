fce9b62342e9a45d2c27ce1105399271f331df99
Timothy Karani <kartimothy@gmail.com>
2020-05-15 14:22:47 +0300

    Green level is now playable

--- GameState.gd
+++ GameState.gd
@@ -5,8 +5,7 @@ var num_levels = 2
 var current_level = 1
 
 var title_screen = 'res://ui/HomeScreen.tscn'
-var level1 = 'res://levels/Blue/BlueLevel01.tscn'
-
+var level1 = 'res://levels/Green/levels/GreenLevel01.tscn'
 
 func restart():
 	get_tree().change_scene(title_screen)
