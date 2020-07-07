d82c1d21e0bd113283d9c3059a18052b662c6c50
Timothy Karani <kartimothy@gmail.com>
2020-07-07 21:05:01 +0300

    Android controls now support multitouch

--- ui/Controls.gd
+++ ui/Controls.gd
@@ -9,31 +9,26 @@ signal goup
 signal godown
 
 
+func _on_LeftButton_pressed():
+	emit_signal("goingleft")
+
+func _on_LeftButton_released():
+	emit_signal("notgoingleft")
+
+func _on_RightButton_pressed():
+	emit_signal("goingright")
+
+func _on_RightButton_released():
+	emit_signal("notgoingright")
+
+func _on_TopButton_pressed():
+	emit_signal("goup")
+
+func _on_TopButton_released():
+	pass # Replace with function body.
 
 func _on_BottomButton_pressed():
 	emit_signal("godown")
 
-
-func _on_LeftButton_button_down():
-	emit_signal("goingleft")
-
-
-func _on_RightButton_button_down():
-	emit_signal("goingright")
-
-
-func _on_RightButton_button_up():
-	emit_signal("notgoingright")
-
-
-func _on_LeftButton_button_up():
-	emit_signal("notgoingleft")
-
-
-func _on_TopButton_pressed():
-#	emit_signal("goup")
-	pass
-
-
-func _on_TopButton_button_down():
-	emit_signal("goup")
+func _on_BottomButton_released():
+	pass # Replace with function body.
